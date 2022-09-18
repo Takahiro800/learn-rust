1febb12d32aef2612af4dda2569b344894fa4a5d
taemath <hirotaka081630@gmail.com>
2022-09-18 16:54:51 +0900

    feat: dSL_1_A

---
+++ aoj/README.md
@@ -0,0 +1,15 @@
+# Aizu Online JudgeをRustで埋める
+
+## 毎回の作業
+1. fileを作る
+   1. `touch src/**.rs`
+2. Cargo.tomlに設定を加える
+   1. snipet追加済
+3. inputをマクロを使えないので、alfredからmacroを貼り付ける(aoj)
+4. テストする
+   1. `cargo run --bin (binのname)`
+
+
+# DSL_2_c Range Search(領域探索)
+### 参考
+[Run #5124703 < hanare < Solutions | Aizu Online Judge](https://onlinejudge.u-aizu.ac.jp/solutions/problem/DSL_2_C/review/5124703/hanare/Rust)
---
+++ aoj/src/dsl-1a.rs
@@ -0,0 +1,127 @@
+#![allow(unused_macros)]
+#![allow(non_snake_case)]
+// ----------------------------------------------------------------------------------------------------
+// input macro by @tanakh https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+// ----------------------------------------------------------------------------------------------------
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+// ----------------------------------------------------------------------------------------------------
+// main code
+// ----------------------------------------------------------------------------------------------------
+
+struct UnionFind {
+    par: Vec<usize>,
+    rank: Vec<usize>,
+}
+
+impl UnionFind {
+    fn new(n: usize) -> Self {
+        UnionFind {
+            par: (0..n).collect(),
+            rank: vec![0; n],
+        }
+    }
+
+    fn find(&mut self, x: usize) -> usize {
+        if self.par[x] == x {
+            return x;
+        }
+        self.par[x] = self.find(self.par[x]);
+        self.par[x]
+    }
+
+    fn unite(&mut self, x: usize, y: usize) {
+        let x = self.find(x);
+        let y = self.find(y);
+        if x == y {
+            return;
+        }
+        if self.rank[x] < self.rank[y] {
+            self.par[x] = y;
+        } else {
+            self.par[y] = x;
+            if self.rank[x] == self.rank[y] {
+                self.rank[x] += 1;
+            }
+        }
+    }
+
+    fn same(&mut self, x: usize, y: usize) -> bool {
+        self.find(x) == self.find(y)
+    }
+}
+
+fn main() {
+    input! {
+      N: usize,
+      Q: usize,
+      PAB_vec: [(usize, usize, usize); Q],
+    };
+
+    let mut uf = UnionFind::new(N);
+
+    for (P, A, B) in PAB_vec {
+        match P {
+            0 => uf.unite(A, B),
+            1 => {
+                if uf.same(A, B) {
+                    println!("1");
+                } else {
+                    println!("0");
+                }
+            }
+            _ => (),
+        }
+    }
+}
