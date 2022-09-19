d24c0cfa2f8f4b58829f2d082ffe93e5689d4650
taemath <hirotaka081630@gmail.com>
2022-09-19 14:18:33 +0900

    test(disk.rs): add test

--- minidb/src/disk.rs
+++ minidb/src/disk.rs
@@ -7,6 +7,8 @@ use std::{
 
 pub const PAGE_SIZE: usize = 4096;
 
+// #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, FromBytes, AsBytes)]
+#[derive(Clone, Copy)]
 pub struct PageId(pub u64);
 
 impl PageId {
@@ -67,5 +69,43 @@ impl DiskManager {
         self.heap_file.seek(SeekFrom::Start(offset))?;
         // データを書き込み
         self.heap_file.write_all(data)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::NamedTempFile;
+
+    #[test]
+    fn test() {
+        let (data_file, data_file_path) = NamedTempFile::new().unwrap().into_parts();
+
+        let mut disk = DiskManager::new(data_file).unwrap();
+
+        let mut hello = Vec::with_capacity(PAGE_SIZE);
+
+        hello.extend_from_slice(b"hello");
+        hello.resize(PAGE_SIZE, 0);
+
+        let hello_page_id = disk.allocate_page();
+        disk.write_page_data(hello_page_id, &hello).unwrap();
+
+        let mut world = Vec::with_capacity(PAGE_SIZE);
+        world.extend_from_slice(b"world");
+        world.resize(PAGE_SIZE, 0);
+
+        let world_page_id = disk.allocate_page();
+        disk.write_page_data(world_page_id, &world).unwrap();
+        drop(disk);
+
+        let mut disk2 = DiskManager::open(&data_file_path).unwrap();
+        let mut buf = vec![0; PAGE_SIZE];
+
+        disk2.read_page_data(hello_page_id, &mut buf).unwrap();
+        assert_eq!(hello, buf);
+
+        disk2.read_page_data(world_page_id, &mut buf).unwrap();
+        assert_eq!(world, buf);
     }
 }
