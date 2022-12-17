5bf29059885a4f2731c3edbfecb88c9d7cd3e609
taemath <hirotaka081630@gmail.com>
2022-12-17 22:25:13 +0900

    feat(rust in action/p.15~16): ぶら下がりポインタの作成を試みる

---
+++ Rust-in-Action/ch01/ch01-cereals/src/main.rs
@@ -0,0 +1,18 @@
+// ぶら下がりポインタの例
+
+#[derive(Debug)]
+enum Cereal {
+    Barley,
+    Millet,
+    Rice,
+    Rye,
+    Spelt,
+    Wheat,
+}
+
+fn main() {
+    let mut grains: Vec<Cereal> = vec![];
+    grains.push(Cereal::Rye);
+    drop(grains);
+    println!("{:?}", grains);
+}
