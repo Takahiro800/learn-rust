078f3f177e403284db98ec9459a3e94500854f45
taemath <hirotaka081630@gmail.com>
2022-12-18 10:04:14 +0900

    feat(rust in action/p.52~53): サードパーティライブラリから利用できる型にアクセスする

--- Rust-in-Action/ch02/complex/src/main.rs
+++ Rust-in-Action/ch02/complex/src/main.rs
@@ -1,3 +1,18 @@
+use num::complex::Complex; // useキーワードでComplex型をローカルスコープにインポートする
+
 fn main() {
-    println!("Hello, world!");
+    let a = Complex { re: 2.1, im: -1.2 }; //　どのRust型にもリテラル型が存在する
+    let b = Complex::new(11.1, 22.2); // ほとんどの型で静的メソッド new()が実装されている
+    let result = a + b;
+
+    println!("{} + {}i", result.re, result.im);
 }
+
+/* 出力結果
+13.2 + 21i
+*/
+
+/*
+* Rustにはコンストラクタがないが、どの型にもリテラル形式が存在する。
+    型の名前（Complex）の後に、波括弧で囲んだフィールド(re, im)を並べ、それらに値を代入すれば、その型を初期化できる
+*/
