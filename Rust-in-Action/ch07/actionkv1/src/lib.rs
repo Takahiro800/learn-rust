30710736de86b846213a15fb95ca32e3e8cd83ef
taemath <hirotaka081630@gmail.com>
2023-01-09 06:50:27 +0900

    feat(rust in action/p.261~264): actionKV構造体を初期化する

---
+++ Rust-in-Action/ch07/actionkv1/src/lib.rs
@@ -0,0 +1,62 @@
+use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
+use std::io;
+use std::io::prelude::*;
+use std::io::{BufReader, BufWriter, SeekFrom};
+use std::path::Path;
+
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use crc::crc32;
+use serde_derive::{Deserialize, Serialize};
+
+type ByteString = Vec<u8>;
+
+type ByteStr = [u8];
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct KeyValuePair {
+    pub key: ByteString,
+    pub value: ByteString,
+}
+
+#[derive(Debug)]
+pub struct ActionKV {
+    f: File,
+    pub index: HashMap<ByteString, u64>,
+}
+
+impl ActionKV {
+    pub fn open(path: &Path) -> io::Result<Self> {
+        let f = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .append(true)
+            .open(path)?;
+
+        let index = HashMap::new();
+        Ok(ActionKV { f, index })
+    }
+
+    // line 79~
+    pub fn load(&mut self) -> io::Result<()> {
+        let mut f = BufReader::new(&mut self.f);
+
+        loop {
+            let position = f.seek(SeekFrom::Current(0));
+
+            let maybe_kv = ActionKV::process_record(&mut f);
+
+            let kv = match maybe_kv {
+                Ok(kv) => kv,
+                Err(err) => match err.kind() {
+                    io::ErrorKind::UnexpectedEof => {
+                        break;
+                    }
+                    _ => return Err(err),
+                },
+            };
+        }
+        Ok(())
+    }
+}
